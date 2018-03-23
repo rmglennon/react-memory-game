749bb6d78df858c5c87465a326b12a1f7b4f0e43
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-22 22:08:08 -0700

    get cards rendering on page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Wrapper from "./components/Wrapper";
 import Card from "./components/Card";
-import Header from "./components/Header";
-import Title from "./components/Title";
-import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+// import Header from "./components/Header";
+// import Page from "./components/Page";
+// import Footer from "./components/Footer";
 import friends from "./friends.json";
 import "./App.css";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <h1>Friends List</h1>
+        {this.state.friends.map(friend => (
+          <Card
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
