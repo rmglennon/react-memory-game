263fbf2b0b05bbefd99f900f2591bb359b56d529
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-24 22:43:03 -0700

    rename files, add header, try to figure out react

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
 import React, { Component } from 'react';
 import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
-// import Header from "./components/Header";
-// import Page from "./components/Page";
+import Header from "./components/Header";
 // import Footer from "./components/Footer";
-import friends from "./friends.json";
+import cards from "./cards.json";
 import "./App.css";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    cards: cards,
+    topScore: 0,
+    currentScore: 0
   };
 
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
+    const friends = this.state.cards.filter(card => card.id !== id);
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    this.setState({ cards });
   };
+  
+  checkIfClicked = id => {
+    
+  }
+  
+  
+  // shuffleCards = id => {
+  // 
+  //   cards.sort(function(a, b){return 0.5 - Math.random()})
+  // 
+  //   this.setState({ friends });
+  // };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
+      <div className="container">
+      <Header><h1>Memory Card Game</h1>
+      </Header>
+
       <Wrapper>
-        <h1>Friends List</h1>
-        {this.state.friends.map(friend => (
+        
+        {this.state.cards.map(card => (
           <Card
             removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+            id={card.id}
+            key={card.id}
+            image={card.image}
           />
         ))}
       </Wrapper>
+      </div>
     );
   }
 }
