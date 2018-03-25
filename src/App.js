ae6cc4105e4f94d3ad1693f816b997b7616847eb
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-25 15:35:34 -0700

    move sort function to clicked is true and add ternary for scoring

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ class App extends Component {
   
   checkIfClicked(id) {
     let clickedCard = this.state.cards.filter(card => card.id === id)[0];
-    //console.log("the clickedCard ", clickedCard);
-    let cardsCopy = this.state.cards.slice();
+
+    let cardsCopy = this.state.cards.slice().sort(function(a, b){return 0.5 - Math.random()});
     if (!clickedCard.clicked) {
       clickedCard.clicked = true;
       //console.log(clickedCard);
@@ -34,10 +34,13 @@ class App extends Component {
       console.log(cardsCopy);
       this.setState({
         cards: cardsCopy,
+        topScore: (this.state.currentScore >= this.state.topScore ? this.state.currentScore : this.state.topScore),
+        currentScore: this.state.currentScore + 1,
       });
     }
+    
     else {
-      cardsCopy.sort(function(a, b){return 0.5 - Math.random()});
+      // cardsCopy.sort(function(a, b){return 0.5 - Math.random()});
       let resetCardsCopy = cardsCopy.map((card) => {
         return {
           id: card.id,
@@ -48,25 +51,12 @@ class App extends Component {
       console.log(resetCardsCopy);
       this.setState({
         cards: resetCardsCopy,
+        // topScore:
+        currentScore: 0,
       });
     } 
-    // this.setState({
-    //   clicked: true
-    // });
     console.log("clicked!  " + id);
   }
-  
-//   removeFriend = id => {
-//   // Filter this.state.friends for friends with an id not equal to the id being removed
-//   const friends = this.state.cards.filter(card => card.id !== id);
-//   // Set this.state.friends equal to the new friends array
-//   this.setState({ cards });
-// };
-  
-  // TODO: if clicked is true, then shuffle cards
-  
-
-  //   cards.sort(function(a, b){return 0.5 - Math.random()})
 
   
   // Map over this.state.cards and render a Card component for each object
